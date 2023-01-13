cb4132a246212463d0c3939f1f27f12996bdfecc
Hau67 <dinhhau671012@gmail.com>
2023-01-13 19:27:50 +0700

    bai_tap

--- baitap/baitap4/ss4_bai_tap_1.sql
+++ baitap/baitap4/ss4_bai_tap_1.sql
@@ -1,6 +1,7 @@
 use quan_ly_sinh_vien;
 -- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
-select * from subject 
+select * 
+from subject 
 where subject.credit >= all (select credit from subject)
 ;
 
